724e20695d48034d0d4fabb647390729c8c61acf
npech <n.pechenkov1@mail.ru>
2024-07-12 16:45:04 +0300

    gotovo

--- module_2_4.py
+++ module_2_4.py
@@ -7,14 +7,15 @@ for i in numbers:
     if i < 2:
         is_Prime = False
     else:
-        for j in numbers:
-            if (i % j) == 0:
+        for j in range(2, int(i) + 1):
+            if (i % j // 2) == 0:
                 is_Prime = True
+            else:
+                is_Prime = False
 
-    if is_Prime == True:
+    if is_Prime:
         primes.append(i)
-
-    else:
+    elif is_Prime == False:
         not_primes.append(i)
 
 print('Простые числа: ', primes)
